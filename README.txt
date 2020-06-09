004780a812292c13f389c3ca3b1308504faa313a
97mark749 <markkirby27@gmail.com>
2020-06-09 19:50:42 -0400

    added pandas framework

--- README.txt
+++
@@ -1,1 +0,0 @@
-Repository of python data visualization
--- bar-graph-with-csv.py
+++ bar-graph-with-csv.py
@@ -2,9 +2,10 @@ from matplotlib import pyplot as plt
 import numpy as np
 import csv
 from collections import Counter
+import pandas as pd
 
 #  print(plt.style.available)
-# execute the abaove to show a list of options for styles
+# execute the above to show a list of options for styles
 # Note some style have their own grid and color preferences
 
 # This is what to type in for styles
@@ -12,19 +13,35 @@ plt.style.use('fivethirtyeight')
 # plt.xkcd() # Comic book style
 
 # to load in a csv file
-with open('data.csv') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+data = pd.read_csv('data.csv')
+ids = data['Responder_id']
+lang_responses = data['LanguagesWorkedWith']
 
-    language_counter = Counter()
+language_counter = Counter()
 
-    for row in csv_reader:
-        language_counter.update(row['LanguagesWorkedWith'].split(';'))
+for response in lang_responses:
+    language_counter.update(response.split(';'))
 
     #row = next(csv_reader)
     # To split the values from the semi colon just use the split method
-    print(row['LanguagesWorkedWith'].split(';'))
-    
-print(language_counter.most_common(15))
+    #gprint(row['LanguagesWorkedWith'].split(';'))
+# Prints the results fo the counter and splits it up to the 15 most common
+
+languages = []
+popularity = []
+
+for item in language_counter.most_common(15):
+    languages.append(item[0])
+    popularity.append(item[1])
+
+languages.reverse()
+popularity.reverse()
+plt.barh(languages,popularity)
+
+
+print(languages,"\n",popularity)
+
+#print(language_counter.most_common(15))
 """
 A format String consists of a part for color, marker, and line:
     fmt = '[marker][line][color]'
@@ -32,18 +49,18 @@ A format String consists of a part for color, marker, and line:
 
 
 # # Title
-# plt.title("Median Salary (USD) By Age")
+plt.title("Programming Language Popularity")
 #
-# # Axis Labels
-# plt.xlabel("Ages")
-# plt.ylabel("Median Salary (USD)")
+# Axis Labels
+plt.ylabel("Programming Languages")
+plt.xlabel("Number of People Who Use it")
 #
 # # Add a Legend
 # # plt.legend(["All Devs", "Py Devs"])
 # # or
 # plt.legend()
 # # Below will adjust the plot parameters to give some padding
-# plt.tight_layout()
+plt.tight_layout()
 # Adds a grid to the graph
 # plt.grid(True)
 
